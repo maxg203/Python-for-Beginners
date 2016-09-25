cc242e9cfd66c42aaaeba3b5c740aef33ff1a060
YouTube <YouTube@Maxs-Macbook-Pro-Retina.local>
2016-09-25 19:21:43 +0100

    Source code up to Python for Beginners Part 12

--- bmr.py
+++ bmr.py
@@ -1,9 +1,25 @@
+# Basal Metabolic Rate Calculator
 
+weight = int(input("Enter your weight in KG: \n"))
+height = int(input("Enter your height in cm: \n"))
+age = int(input("Enter your age in years: \n"))
+isMale = str(input("Are you male? (y/n)"))
 
-weight = 82
-height = 185
-age = 18
+if isMale == "y":
+    isMale = True
+elif isMale == "n":
+    isMale = False
+else:
+    print("Error")
+    quit()
+    
+    
 
-bmr = 66.5 + (13.75 * weight) + (5 * height) - (6.755 * age)
+# Mifflin St. Jeor Equation for males
+if isMale:
+    bmr = 66.5 + (13.75 * weight) + (5 * height) - (6.755 * age)
+else:
+    bmr = 655.1 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+
 bmr = round(bmr)
 print(bmr)
---
+++ loops.py
@@ -0,0 +1,16 @@
+
+"""
+# For loop
+numbers = range(1,11)
+
+for number in numbers:
+    print(number)
+"""
+
+# While loop
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
