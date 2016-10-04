6ba484a424a7c1567ca4ef1afc4388aa61dfa1b9
YouTube <YouTube@Maxs-Macbook-Pro-Retina.local>
2016-10-05 00:06:07 +0100

    Source code up to part 19

---
+++ tuples.py
@@ -0,0 +1,17 @@
+
+# List
+numbers = [1,2,3,4,5]
+
+# Tuple
+squares = (1,4,16,25,36)
+
+
+# Variable unpacking
+a,b,c = [1,2,3]
+print(a,b,c)
+
+
+x = (1,)
+y = (1,2)
+z = (1,2,3)
+print(x,y,z)
---
+++ using_modules.py
@@ -0,0 +1,7 @@
+import math
+import functions, pillow
+
+print(math.floor(3.7))
+
+for each in range(1,11):
+    print(functions.square_area(each))
