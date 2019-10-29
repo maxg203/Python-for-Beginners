992276a0b09caeb504d4b173c6f540afd1642a29
rounak176 <32223619+rounak176@users.noreply.github.com>
2019-10-30 01:34:09 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,16 +1,53 @@
+"""
+### For loop
+int_list = [1, 2, 3, 4, 5, 6]
+sum = 0
+for iter in int_list:
+    sum += iter
+print("Sum =", sum)
+print("Avg =", sum/len(int_list))
+
+##Output :
+#Sum = 21
+#Avg = 3.5
+"""
 
 """
-# For loop
-numbers = range(1,11)
-
-for number in numbers:
-    print(number)
+### Nested For loop
+for x in range(1,5): 
+  for y in range(1,5): 
+    print(x*y)
+    
+##Output :
+#1
+#2
+#3
+#4
+#2
+#4
+#6
+#8
+#3
+#6
+#9
+#12
+#4
+#8
+#12
+#16
 """
 
-# While loop
+### While Loop
+fruits = ["banana", "apple", "orange", "kiwi"]
+position = 0
+while position < len(fruits):
+    print(fruits[position])
+    position = position + 1
+print("reached end of list")
 
-count = 0
-
-while count < 5:
-    print(count)
-    count += 1
+##Output :
+#banana
+#apple
+#orange
+#kiwi
+#reached end of list
